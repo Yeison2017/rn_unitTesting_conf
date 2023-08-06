e597fe5f1511816517e8cfee5548fce55e0e55ee
Yeison Hernandez <developer.ss.2@tucash.co>
2023-08-06 15:10:54 -0500

    Pruebas en 11-async-await

---
+++ src/base-pruebas/11-async-await.ts
@@ -0,0 +1,17 @@
+export const getImagen = async () => {
+  try {
+    const apiKey = "hSxo6W0KC7zNGxRoAVbL9wBi59URVgzS";
+    const resp = await fetch(
+      `http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
+    );
+    const { data } = await resp.json();
+
+    const { url } = data.images.original;
+    return url;
+  } catch (error) {
+    // console.error(error);
+    return "No se encontro la imagen";
+  }
+};
+
+getImagen();
